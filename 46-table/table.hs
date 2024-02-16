aadd6e61a9aa12067aca557dcce7de8bbc311d0f
adrian052 <a_drian1@hotmail.es>
2024-02-16 10:25:45 -0600

    Problem 47 solved

--- 46-table/table.hs
+++ 46-table/table.hs
@@ -1,10 +1,2 @@
-and' :: Bool -> Bool -> Bool
-and' True True = True
-and' _ _ = False
-
-or' :: Bool -> Bool -> Bool
-or' False False = False
-or' _ _ = True
-
 table :: (Bool -> Bool -> Bool) -> [(Bool, Bool,Bool)]
 table expr = [(a,b,expr a b)| a <- [True, False], b <- [True, False]]
