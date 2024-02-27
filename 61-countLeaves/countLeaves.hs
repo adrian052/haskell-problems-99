9c203d3cec909cac47a027862ee39db7734d92bd
adrian052 <a_drian1@hotmail.es>
2024-02-27 08:49:18 -0600

    Problem 61A solved

--- 61-countLeaves/countLeaves.hs
+++ 61-countLeaves/countLeaves.hs
@@ -10,3 +10,9 @@ countLeaves Empty = 0
 countLeaves (Branch _ l r) = if l == Empty && r == Empty
                                 then 1
                                 else countLeaves l + countLeaves r
+
+leaves :: Eq a => Tree a -> [a]
+leaves Empty            = []
+leaves (Branch x l r)   = if l == Empty && r == Empty
+                            then [x]
+                            else leaves l ++ leaves r
