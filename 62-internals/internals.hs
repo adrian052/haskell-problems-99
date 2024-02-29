0c25afdd819e8846541a201161d3c10c1bef0b03
adrian052 <a_drian1@hotmail.es>
2024-02-29 09:02:37 -0600

    Problem 62B solved

--- 62-internals/internals.hs
+++ 62-internals/internals.hs
@@ -10,3 +10,13 @@ internals Empty            = []
 internals (Branch x l r)   = if l == Empty && r == Empty
                             then []
                             else [x] ++ internals l ++ internals r
+
+
+atLevel :: Eq a => Tree a -> Int -> [a]
+atLevel tree lvl = atLevel' tree 1 lvl
+
+atLevel':: Eq a => Tree a -> Int -> Int -> [a]
+atLevel' Empty _ _                  = []
+atLevel' (Branch x l r) currLvl lvl = if currLvl == lvl 
+                                    then [x]
+                                    else atLevel' l (currLvl+1) lvl ++  atLevel' r (currLvl+1) lvl
