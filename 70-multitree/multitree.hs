05799f412279e03b62d8a376a608ae75e2ba8608
adrian052 <a_drian1@hotmail.es>
2024-03-08 16:58:43 -0600

    Problem 70 solved

---
+++ 70-multitree/multitree.hs
@@ -0,0 +1,20 @@
+data Tree a = Node a [Tree a]
+        deriving (Eq, Show)
+
+tree1 = Node 'a' []
+
+tree2 = Node 'a' [Node 'b' []]
+
+tree3 = Node 'a' [Node 'b' [Node 'c' []]]
+
+tree4 = Node 'b' [Node 'd' [], Node 'e' []]
+
+tree5 = Node 'a' [
+                Node 'f' [Node 'g' []],
+                Node 'c' [],
+                Node 'b' [Node 'd' [], Node 'e' []]
+                ]
+
+nnodes :: Tree a -> Int
+nnodes (Node a []) = 1
+nnodes (Node a xs) = foldl (\acc x -> acc+(nnodes x)) 1 xs 
